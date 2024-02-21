67fe99cfabe0a59651d2565de132a52ed595330b
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-21 08:47:09 +0100

    2-6-Build the new grant page

--- app/grants/page.tsx
+++ app/grants/page.tsx
@@ -1,8 +1,13 @@
 import { Button } from "@radix-ui/themes";
+import Link from "next/link";
 import React from "react";
 
 const GrantsPage = () => {
-  return <Button>New Grant</Button>;
+  return (
+    <Button>
+      <Link href="/grants/new">New Grant</Link>
+    </Button>
+  );
 };
 
 export default GrantsPage;
