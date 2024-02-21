233bf49b316120962db3898e958375e7ce9c4430
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-21 21:06:22 +0100

    2-9-Handle form submission

--- app/grants/new/page.tsx
+++ app/grants/new/page.tsx
@@ -1,17 +1,46 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+
+import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+// import dynamic from "next/dynamic";
+// const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+//   ssr: false,
+// });
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface GrantForm {
+  title: string;
+  description: string;
+}
 
 const NewGrantPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<GrantForm>();
+
   return (
-    <div className="max-w-xl space-y-2">
+    <form
+      className="max-w-xl space-y-2"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/grants", data);
+        router.push("/grants");
+      })}
+    >
       <TextField.Root>
-        <TextField.Input placeholder="Title" />
+        <TextField.Input placeholder="Title" {...register("title")} />
       </TextField.Root>
-      <SimpleMDE placeholder="Description" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Grant</Button>
-    </div>
+    </form>
   );
 };
 
