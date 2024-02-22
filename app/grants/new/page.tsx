9163571ce14865f6fe9590e530677364094d9b0e
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-22 06:12:37 +0100

    2-11-Extract the ErrorMessage component

--- app/grants/new/page.tsx
+++ app/grants/new/page.tsx
@@ -14,6 +14,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createGrantSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/components/ErrorMessage";
 
 type GrantForm = z.infer<typeof createGrantSchema>;
 
@@ -50,11 +51,7 @@ const NewGrantPage = () => {
         <TextField.Root>
           <TextField.Input placeholder="Title" {...register("title")} />
         </TextField.Root>
-        {errors.title && (
-          <Text color="red" as="p">
-            {errors.title.message}
-          </Text>
-        )}
+        {<ErrorMessage>{errors.title?.message}</ErrorMessage>}
         <Controller
           name="description"
           control={control}
@@ -62,11 +59,7 @@ const NewGrantPage = () => {
             <SimpleMDE placeholder="Description" {...field} />
           )}
         />
-        {errors.description && (
-          <Text color="red" as="p">
-            {errors.description.message}
-          </Text>
-        )}
+        {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
 
         <Button>Submit New Grant</Button>
       </form>
