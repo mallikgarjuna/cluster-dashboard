9ba199e556cd31f6ba3f09f943180249dbb9b085
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-22 20:50:20 +0100

    3-1-Show the grants

--- app/grants/page.tsx
+++ app/grants/page.tsx
@@ -1,12 +1,48 @@
-import { Button } from "@radix-ui/themes";
+import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import React from "react";
+import prisma from "@/prisma/client";
 
-const GrantsPage = () => {
+const GrantsPage = async () => {
+  const grants = await prisma.grant.findMany();
+
   return (
-    <Button>
-      <Link href="/grants/new">New Grant</Link>
-    </Button>
+    <div>
+      <div className="mb-5">
+        <Button>
+          <Link href="/grants/new">New Grant</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Grant</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {grants.map((grant) => (
+            <Table.Row key={grant.id}>
+              <Table.Cell>
+                {grant.title}
+                <div className="block md:hidden">{grant.status}</div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {grant.status}
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                {grant.createdAt.toDateString()}
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </div>
   );
 };
 
