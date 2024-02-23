55928f673e2a7a88ff151f84809984bb009b7ad3
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-23 16:15:14 +0100

    3-9-Disable SSR for loading md editor

--- app/grants/new/page.tsx
+++ app/grants/new/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { Button, Callout, Text, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
-// import dynamic from "next/dynamic";
-// const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
-//   ssr: false,
-// });
+// import SimpleMDE from "react-simplemde-editor";
+import dynamic from "next/dynamic";
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
