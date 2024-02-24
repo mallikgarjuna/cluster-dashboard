b76c63bfb51923ec955424d461989df1f2cc2bac
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-24 05:39:15 +0100

    4-1-Add the edit button

--- app/grants/[id]/page.tsx
+++ app/grants/[id]/page.tsx
@@ -1,6 +1,8 @@
 import GrantStatusBadge from "@/app/components/GrantStatusBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 
@@ -18,16 +20,24 @@ const GrantDetailPage = async ({ params }: Props) => {
   if (!grant) notFound();
 
   return (
-    <div>
-      <Heading>{grant.title}</Heading>
-      <Flex gap="3" my="2">
-        <GrantStatusBadge status={grant.status} />
-        <Text>{grant.updatedAt.toDateString()}</Text>
-      </Flex>
-      <Card className="prose" mt="4">
-        <ReactMarkdown>{grant.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Box>
+        <Heading>{grant.title}</Heading>
+        <Flex gap="3" my="2">
+          <GrantStatusBadge status={grant.status} />
+          <Text>{grant.updatedAt.toDateString()}</Text>
+        </Flex>
+        <Card className="prose" mt="4">
+          <ReactMarkdown>{grant.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/grants/${grant.id}/edit`}>Edit Grant</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
