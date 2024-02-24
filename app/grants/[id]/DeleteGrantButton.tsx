f91d466946decd21b3f6f26469d48b940d3d6798
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-24 12:45:41 +0100

    5-5-Handle errors

--- app/grants/[id]/DeleteGrantButton.tsx
+++ app/grants/[id]/DeleteGrantButton.tsx
@@ -2,7 +2,7 @@
 import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   grantId: number;
@@ -10,38 +10,65 @@ interface Props {
 
 const DeleteGrantButton = ({ grantId }: Props) => {
   const router = useRouter();
+  const [error, setError] = useState(false);
+
+  const deleteGrant = async () => {
+    try {
+      // to simuate error
+      // throw new Error();
+      await axios.delete(`/api/grants/${grantId}`);
+      router.push("/grants");
+      router.refresh();
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Grant</Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletetion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delet this grant? This action cannot be
-          undone.
-        </AlertDialog.Description>
-        <Flex mt="4" gap="3">
-          <AlertDialog.Cancel>
-            <Button variant="soft" color="gray">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={async () => {
-                await axios.delete(`/api/grants/${grantId}`);
-                router.push("/grants");
-                router.refresh();
-              }}
-            >
-              Delete Grant
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Grant</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletetion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delet this grant? This action cannot be
+            undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red" onClick={deleteGrant}>
+                Delete Grant
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description>
+            This grant cannot be deleted.
+          </AlertDialog.Description>
+          <Button
+            variant="soft"
+            color="gray"
+            mt="2"
+            onClick={() => {
+              setError(false);
+            }}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
