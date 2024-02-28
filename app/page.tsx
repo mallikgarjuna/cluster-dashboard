979d3e45dcd194a157ce152b2756888a9cfc3dd1
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-28 20:25:19 +0100

    9-4-Lay out the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import LatestGrants from "./LatestGrants";
 import GrantSummary from "./GrantSummary";
 import prisma from "@/prisma/client";
 import GrantChart from "./GrantChart";
+import { Flex, Grid } from "@radix-ui/themes";
 
 export default async function Home() {
   const submitted = await prisma.grant.count({
@@ -13,10 +14,20 @@ export default async function Home() {
   const rejected = await prisma.grant.count({ where: { status: "REJECTED" } });
 
   return (
-    <>
-      <div>Cluster Dashboard</div>
-      {/* <LatestGrants /> */}
-      <GrantChart submitted={submitted} awarded={awarded} rejected={rejected} />
-    </>
+    <Grid columns={{ initial: "1", md: "2" }} gap="5">
+      <Flex direction="column" gap="5">
+        <GrantSummary
+          submitted={submitted}
+          awarded={awarded}
+          rejected={rejected}
+        />
+        <GrantChart
+          submitted={submitted}
+          awarded={awarded}
+          rejected={rejected}
+        />
+      </Flex>
+      <LatestGrants />
+    </Grid>
   );
 }
