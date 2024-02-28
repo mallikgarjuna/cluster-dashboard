9f0e1374ac58661f3c3dab24a473119691e090ab
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-02-28 06:36:23 +0100

    8-7-Build the layout of the pagination component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return <div>Cluster Dashboard</div>;
+  return (
+    <>
+      <div>Cluster Dashboard</div>
+      <Pagination itemsCount={100} pageSize={10} currentPage={1} />
+    </>
+  );
 }
