5322887b28ad37d58aa710230ce67def63abf8c1
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-03-03 07:39:51 +0100

    11-2 Create seed file and test data using Faker for dev env

---
+++ prisma/seed.ts
@@ -0,0 +1,35 @@
+import prisma from "./client";
+import { faker } from "@faker-js/faker";
+
+async function main() {
+  // Don't run seed script in production!!
+  // To secure production db safe: exit if not 'dev' env;
+  console.log(process.env.NODE_ENV); // gives 'undefined', why?
+
+  //   if (process.env.NODE_ENV !== "development") return;
+
+  await prisma.grant.deleteMany();
+
+  console.log("Start seeding...");
+  for (let i = 0; i < 10; i++) {
+    await prisma.grant.create({
+      data: {
+        title: `Grant ${faker.location.country()} `,
+        description: `Description Grant: ${faker.lorem.lines(2)}`,
+        status: "SUBMITTED",
+      },
+    });
+  }
+
+  console.log("Finish seeding.");
+}
+
+main()
+  .catch(async (e) => {
+    console.log(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
