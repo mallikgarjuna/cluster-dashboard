026529ca879a33299c580918cc890eacec8622bd
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-03-31 19:02:35 +0200

    12-4-Create SignupForm validation schema using Zod

--- app/validationSchemas.ts
+++ app/validationSchemas.ts
@@ -44,3 +44,39 @@ export const SignInFormSchema = z.object({
 export type SignInFormInputFieldsType = z.infer<typeof SignInFormSchema>;
 
 export type SignInFormInputFieldsName = keyof SignInFormInputFieldsType;
+
+// Signup form schema
+export const SignupFormSchema = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, "First name must be at least 2 characters")
+      .max(45, "First name must be less than 45 characters")
+      .regex(new RegExp("^[a-zA-Z]+$", "No special characters are allowed!")),
+    lastName: z
+      .string()
+      .min(2, "Last name must be at least 2 characters")
+      .max(45, "Last name must be less than 45 characters")
+      .regex(new RegExp("^[a-zA-Z]+$", "No special characters are allowed!")),
+    email: z
+      .string()
+      .email("Please enter a valid email address")
+      .endsWith("@umcg.nl", "Please enter a valid UMCG email address"),
+    password: z
+      .string()
+      .min(5, "Password must be at least 5 characters long")
+      .max(45, "Password must be less than 45 characters long"),
+    confirmPassword: z
+      .string()
+      .min(5, "Password must be at least 5 characters long")
+      .max(45, "Password must be less than 45 characters long"),
+    accepted: z.literal(true, {
+      errorMap: () => ({
+        message: "Please accept the terms and conditions",
+      }),
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password and confirmPassword do not match",
+    path: ["password", "confirmPassword"],
+  });
