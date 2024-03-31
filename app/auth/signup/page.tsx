bbb8946fa4e0ee7b49b52896e10acbc5326b1c01
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-03-31 18:34:28 +0200

    12-3-Create SingupForm component

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,18 +1,18 @@
 import { Link } from "@/app/components";
 import SignupForm from "@/app/components/auth/SignupForm";
-import { Box, Flex, Grid } from "@radix-ui/themes";
+import { Box, Flex, Grid, Text } from "@radix-ui/themes";
 import React from "react";
 import { FaUserPlus } from "react-icons/fa";
 
 const SignupPage = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 justify-center items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 place-items-center items-center gap-3">
       <Box className="md:col-span-2 flex justify-center items-center">
-        <p className="p-2 ">Already have an account?</p>
+        <Text className="p-2 ">Already have an account?</Text>
         <Link href="/auth/signin">Sign in</Link>
       </Box>
       <SignupForm />
-      <FaUserPlus size={100} />
+      <FaUserPlus size={200} />
     </div>
   );
 };
--- app/components/auth/SignupForm.tsx
+++ app/components/auth/SignupForm.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+"use client";
+
+import { Button, Checkbox, Input, Link } from "@nextui-org/react";
+import { HiEye, HiEyeOff, HiKey, HiMail, HiUser } from "react-icons/hi";
+import React, { useState } from "react";
 
 const SignupForm = () => {
-  return <div>SignupForm</div>;
+  const [isVisiblePass, setIsVisiblePass] = useState(false);
+  const toggleVisiblePass = () => setIsVisiblePass((prev) => !prev);
+
+  return (
+    <form className="grid grid-cols-2 gap-3 p-2 place-self-stretch border rounded-md">
+      <Input
+        type="text"
+        label="First Name"
+        placeholder="Enter your first name"
+        startContent={<HiUser />}
+      />
+      <Input
+        type="text"
+        label="Last Name"
+        placeholder="Enter your last name"
+        startContent={<HiUser />}
+      />
+      <Input
+        type="email"
+        label="Email"
+        placeholder="Enter your email"
+        startContent={<HiMail />}
+        className="col-span-2"
+      />
+      <Input
+        type={isVisiblePass ? "text" : "password"}
+        label="Password"
+        placeholder="Enter your password"
+        startContent={<HiKey />}
+        endContent={
+          <Button isIconOnly variant="light" onPress={toggleVisiblePass}>
+            {isVisiblePass ? <HiEyeOff /> : <HiEye />}
+          </Button>
+        }
+        className="col-span-2"
+      />
+      <Input
+        type={isVisiblePass ? "text" : "password"}
+        label="Confirm Password"
+        placeholder="Renter to confirm password"
+        startContent={<HiKey />}
+        className="col-span-2"
+      />
+      <Checkbox type="checkbox" className="col-span-2">
+        I accept the <Link href="/terms"> terms and conditions</Link>.
+      </Checkbox>
+      <div className="col-span-2 flex justify-center">
+        <Button type="submit" color="primary" className="w-48">
+          Sign up
+        </Button>
+      </div>
+    </form>
+  );
 };
 
 export default SignupForm;
