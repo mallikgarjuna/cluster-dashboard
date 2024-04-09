d569db4ddef895e2e66cf97c1b86b7203fec72e7
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-04-09 06:58:46 +0200

    13-6-Create signJwt and verifyJwt functions using JWT

---
+++ lib/jwt.ts
@@ -0,0 +1,31 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface SignOption {
+  expiresIn: string | number;
+}
+
+const DEFAULT_SIGN_OPTION: SignOption = {
+  expiresIn: "1d",
+};
+
+// function for signing jwt token or encrypting payload
+export function signJwt(
+  payload: JwtPayload,
+  option: SignOption = DEFAULT_SIGN_OPTION
+) {
+  const secretKey = process.env.JWT_USER_ID_SECRET_KEY!;
+  const token = jwt.sign(payload, secretKey, option);
+  return token;
+}
+
+// function for verifying and decoding jwt token
+export function verifyJwt(token: string) {
+  try {
+    const secretKey = process.env.JWT_USER_ID_SECRET_KEY!;
+    const decoded = jwt.verify(token, secretKey);
+    return decoded as JwtPayload;
+  } catch (error) {
+    console.log(error);
+    return new Error("Invalid or expired jwt token");
+  }
+}
