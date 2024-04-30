22bbdc4185da6cd84ee08e99aaf121a90bfd28e8
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-04-30 14:29:23 +0200

    17.11 Creae navbar pages in dashboard

---
+++ app/dashboard/outreach/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OutreachPage = () => {
+  return <div>OutreachPage</div>;
+};
+
+export default OutreachPage;
---
+++ app/dashboard/theses/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ThesesPage = () => {
+  return (
+    <div>ThesesPage</div>
+  )
+}
+
+export default ThesesPage
