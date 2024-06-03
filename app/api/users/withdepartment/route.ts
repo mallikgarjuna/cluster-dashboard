79285492b9fb430610717c9d68d2ff37d398af6f
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-06-03 14:33:05 +0200

    17.40 Filter usersWithDepartment API route with role GOUPLEADER

--- app/api/users/withdepartment/route.ts
+++ app/api/users/withdepartment/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest) {
   const usersWithDepartment = await prisma.user.findMany({
+    where: { role: "GROUPLEADER" },
     orderBy: { lastName: "asc" },
     include: { relatedDepartment: true },
   });
