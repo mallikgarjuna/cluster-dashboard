fbf016dc5ee0592c788a3c6bbb1761a4be05a81d
Mallikarjuna Gurram <mallikgarjuna@gmail.com>
2024-06-17 17:17:47 +0200

    17.57-4 Disable caching in api/users/route

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,9 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 
+// To disable caching for this route
+export const revalidate = 0;
+
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany({ orderBy: { lastName: "asc" } });
   return NextResponse.json(users);
