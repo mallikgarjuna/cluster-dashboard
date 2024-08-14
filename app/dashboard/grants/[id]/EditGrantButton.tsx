131c3d69cfb541e48daa2f3d1aadff77ed4dab55
Mallik Gurram <mallikgarjuna@gmail.com>
2024-08-14 18:31:21 +0200

    18.12 WIP LM: Add Spinner to EditGrantButtonl.tsx

--- app/dashboard/grants/[id]/EditGrantButton.tsx
+++ app/dashboard/grants/[id]/EditGrantButton.tsx
@@ -1,14 +1,20 @@
+"use client";
+import { Spinner } from "@/app/components";
 import { Pencil2Icon } from "@radix-ui/react-icons";
 import { Button } from "@radix-ui/themes";
 import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
   grantId: string;
 }
 
 const EditGrantButton = ({ grantId }: Props) => {
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <Button>
+    <Button onClick={() => setIsLoading(true)} disabled={isLoading}>
+      {isLoading && <Spinner />}
       <Pencil2Icon />
       <Link href={`/dashboard/grants/edit/${grantId}`}>Edit Grant</Link>
     </Button>
