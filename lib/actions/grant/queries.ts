ecdc6db513f5705569e1bc95b47a5defa8609017
Mallik Gurram <mallikgarjuna@gmail.com>
2024-11-04 22:14:33 +0100

    wip-lm 24.19 Refactor fetchUniqueGrantYearsSA to optimize the prisma query

--- lib/actions/grant/queries.ts
+++ lib/actions/grant/queries.ts
@@ -3,34 +3,46 @@ import prisma from "@/prisma/client";
 
 async function fetchUniqueGrantYearsSA() {
   //   get all unique submission dates (not years, yet) - gives us an array of objects
-  const grantsWithDates = await prisma.grant.findMany({
-    select: { submissionDate: true, projectStartDate: true },
+  const grantsWithSubmissionDates = await prisma.grant.findMany({
+    select: { submissionDate: true },
     distinct: ["submissionDate"],
   });
 
-  //   get all years from the submission dates
-  const grantYears = grantsWithDates.map((grantObj) =>
-    Object.values(grantObj)
-      .map((dateVal) => (dateVal ? dateVal.getFullYear() : null))
-      .filter(Boolean),
-  );
+  const grantsWithStartDates = await prisma.grant.findMany({
+    select: { projectStartDate: true },
+    distinct: ["projectStartDate"],
+  });
 
-  const grantYearsAll = grantYears
-    .flat() //default: flat(1) - or use flat(Infinity) to flatten all levels
+  // Get all years
+  const grantSubmissionYearsAll = grantsWithSubmissionDates
+    .map((grantObj) =>
+      grantObj.submissionDate ? grantObj.submissionDate.getFullYear() : null,
+    )
     .filter(Boolean)
     .sort((a, b) => (a ?? 0) - (b ?? 0)); // Ensure a and b are not null
 
-  // const grantYearsAll = grantsWithDates
-  //   .map((grantObj) =>
-  //     grantObj.submissionDate ? grantObj.submissionDate.getFullYear() : null,
-  //   )
-  //   .filter(Boolean)
-  //   .sort((a, b) => (a ?? 0) - (b ?? 0)); // Ensure a and b are not null
+  const grantStartYearsAll = grantsWithStartDates
+    .map((grantObj) =>
+      grantObj.projectStartDate
+        ? grantObj.projectStartDate.getFullYear()
+        : null,
+    )
+    .filter(Boolean)
+    .sort((a, b) => (a ?? 0) - (b ?? 0)); // Ensure a and b are not null
 
   // get unique years
-  const uniqueGrantSubmissionYears = Array.from(new Set(grantYearsAll));
+  const uniqueGrantSubmissionYears = Array.from(
+    new Set(grantSubmissionYearsAll),
+  );
 
-  return uniqueGrantSubmissionYears;
+  const uniqueGrantStartYears = Array.from(new Set(grantStartYearsAll));
+
+  // grantYearsAll
+  const grantYearsAll = Array.from(
+    new Set([...uniqueGrantSubmissionYears, ...uniqueGrantStartYears]),
+  );
+
+  return grantYearsAll;
 }
 
 // Export functions for use in other files
