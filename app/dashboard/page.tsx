b93cb641c9ab0326f350468b3ab1122016240e4d
Mallik Gurram <mallikgarjuna@gmail.com>
2024-11-05 06:35:34 +0100

    wip-lm 24.24 Comment out the totalFundingAwarded in DashboardPage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -338,54 +338,55 @@ export default async function DashboardPage({ searchParams }: Props) {
   const latestGrants = await getLatestGrantsForUser(filters.groupLeader);
 
   // per year grant count data for GrantsSubmittedPerYearChart
-  // const grantsCountPerYearData = [];
-  // for (const year of uniqueGrantSubmissionYears) {
-  //   const submitted = await getSubmittedCountForUser(
-  //     filters.groupLeader,
-  //     year?.toString(),
-  //   );
-  //   const awarded = await getAwardedCountForUser(
-  //     filters.groupLeader,
-  //     year?.toString(),
-  //   );
-  //   const totalFundingAwarded = (
-  //     await getAwardedGrantsForUser(filters.groupLeader, year?.toString())
-  //   ).reduce(
-  //     (accumulator, grant) => accumulator + (grant.budgetAssignedToPI ?? 0),
-  //     0,
-  //   );
+  const grantsCountPerYearData = [];
+  for (const year of uniqueGrantSubmissionYears) {
+    const submitted = await getSubmittedCountForUser(
+      filters.groupLeader,
+      year?.toString(),
+    );
+    const awarded = await getAwardedCountForUser(
+      filters.groupLeader,
+      year?.toString(),
+    );
+    const totalFundingAwarded = 0;
+    // const totalFundingAwarded = (
+    //   await getAwardedGrantsForUser(filters.groupLeader, year?.toString())
+    // ).reduce(
+    //   (accumulator, grant) => accumulator + (grant.budgetAssignedToPI ?? 0),
+    //   0,
+    // );
 
-  //   grantsCountPerYearData.push({
-  //     year: year,
-  //     submitted: submitted,
-  //     awarded: awarded,
-  //     totalFundingAwarded: totalFundingAwarded,
-  //   });
-  // }
+    grantsCountPerYearData.push({
+      year: year,
+      submitted: submitted,
+      awarded: awarded,
+      totalFundingAwarded: totalFundingAwarded,
+    });
+  }
 
-  const grantsCountPerYearData = await Promise.all(
-    uniqueGrantSubmissionYears.map(async (year) => {
-      const [submitted, awarded, totalFundingAwarded] = await Promise.all([
-        getSubmittedCountForUser(filters.groupLeader, year?.toString()),
-        getAwardedCountForUser(filters.groupLeader, year?.toString()),
-        getAwardedGrantsForUser(filters.groupLeader, year?.toString()).then(
-          (grants) =>
-            grants.reduce(
-              (accumulator, grant) =>
-                accumulator + (grant.budgetAssignedToPI ?? 0),
-              0,
-            ),
-        ),
-      ]);
+  // const grantsCountPerYearData = await Promise.all(
+  //   uniqueGrantSubmissionYears.map(async (year) => {
+  //     const [submitted, awarded, totalFundingAwarded] = await Promise.all([
+  //       getSubmittedCountForUser(filters.groupLeader, year?.toString()),
+  //       getAwardedCountForUser(filters.groupLeader, year?.toString()),
+  //       getAwardedGrantsForUser(filters.groupLeader, year?.toString()).then(
+  //         (grants) =>
+  //           grants.reduce(
+  //             (accumulator, grant) =>
+  //               accumulator + (grant.budgetAssignedToPI ?? 0),
+  //             0,
+  //           ),
+  //       ),
+  //     ]);
 
-      return {
-        year: year,
-        submitted: submitted,
-        awarded: awarded,
-        totalFundingAwarded: totalFundingAwarded,
-      };
-    }),
-  );
+  //     return {
+  //       year: year,
+  //       submitted: submitted,
+  //       awarded: awarded,
+  //       totalFundingAwarded: totalFundingAwarded,
+  //     };
+  //   }),
+  // );
   // console.log("grantsCountPerYearData: ", grantsCountPerYearData);
 
   // // Get all group leaders - for grantsCountOfPIData for PIGrantsTable
