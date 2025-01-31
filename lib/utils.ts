d39c640b54f164dd7195b67f52e11f32022c3dff
Mallik Gurram <mallikgarjuna@gmail.com>
2025-01-31 15:05:41 +0100

    wip-lm: refactor-wip:27-8 Recover getErrorMessage in /lib/utils.ts from git log previous commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,32 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { ZodError } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getErrorMessage = (error: unknown) => {
+  let message: string;
+
+  if (error instanceof ZodError) {
+    let errorMessage = "";
+    let errors = error.flatten().fieldErrors;
+    for (const [key, value] of Object.entries(errors)) {
+      if (value && value.length > 0) {
+        errorMessage += `${key}: ${value[0]}\n`;
+      }
+    }
+    message = errorMessage;
+  } else if (error instanceof Error) {
+    message = error.message;
+  } else if (error && typeof error === "object" && "message" in error) {
+    message = String(error.message); // or error.message as string;
+  } else if (typeof error === "string") {
+    message = error;
+  } else {
+    message = "Unknown error. Something went wrong.";
+  }
+
+  return message;
+};
