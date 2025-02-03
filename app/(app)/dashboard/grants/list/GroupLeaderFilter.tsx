1e5507dbf2d8198165a1c6c88d8581e6387454c7
Mallik Gurram <mallikgarjuna@gmail.com>
2025-02-03 21:11:58 +0100

    wip-lm: refactor-wip:27-14 Refactor GroupLeaderFilter to use Selector from Shadcn

--- app/(app)/dashboard/grants/list/GroupLeaderFilter.tsx
+++ app/(app)/dashboard/grants/list/GroupLeaderFilter.tsx
@@ -1,6 +1,17 @@
 "use client";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { UserWithDepartment } from "@/prisma/customTypes";
-import { Select, SelectItem, SelectSection } from "@nextui-org/react";
+// import { Select, SelectItem, SelectSection } from "@nextui-org/react";
 import { OSDepartmentShortName } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
@@ -23,25 +34,25 @@ const GroupLeaderFilter = ({ users }: Props) => {
 
   if (!session) return null;
 
-  // If a groupleader logged in, filter by group leader
-  if (session?.user.role === "GROUPLEADER") {
-    return (
-      <Select
-        label="Filter by group leader...."
-        className="max-w-xs"
-        // defaultSelectedKeys={[session.user.id]}
-        selectedKeys={[session.user.id]}
-      >
-        <SelectItem
-          key={session.user.id}
-          value={session.user.id}
-          textValue={session.user.lastName ?? ""}
-        >
-          {session.user.lastName}
-        </SelectItem>
-      </Select>
-    );
-  }
+  // // If a groupleader logged in, filter by group leader
+  // if (session?.user.role === "GROUPLEADER") {
+  //   return (
+  //     <Select
+  //       label="Filter by group leader...."
+  //       className="max-w-xs"
+  //       // defaultSelectedKeys={[session.user.id]}
+  //       selectedKeys={[session.user.id]}
+  //     >
+  //       <SelectItem
+  //         key={session.user.id}
+  //         value={session.user.id}
+  //         textValue={session.user.lastName ?? ""}
+  //       >
+  //         {session.user.lastName}
+  //       </SelectItem>
+  //     </Select>
+  //   );
+  // }
 
   if (!users) return <Skeleton />;
 
@@ -83,101 +94,173 @@ const GroupLeaderFilter = ({ users }: Props) => {
     }
   });
 
+  const handleValueChange = (value: string) => {
+    // console.log("groupLeader value: ", value);
+
+    const params = new URLSearchParams(searchParams);
+
+    if (value === "All") {
+      params.delete("groupLeader");
+    } else {
+      params.set("groupLeader", value);
+    }
+
+    const queryString = params.size ? "?" + params.toString() : "";
+
+    router.push(pathname + queryString);
+  };
+
+  // If a groupleader logged in, filter by group leader
+  const defaultValueSelect =
+    session.user.role === "GROUPLEADER"
+      ? session.user.id
+      : searchParams.get("groupLeader") || "All";
+
   return (
-    <Select
-      items={departmentShortNames as Iterable<object>}
-      label="Filter by group leader...."
-      className="max-w-xs"
-      defaultSelectedKeys={[searchParams.get("groupLeader") || "All"]}
-      onChange={(event) => {
-        const groupLeader = event.target.value;
-        // console.log("groupLeader: ", groupLeader);
-        const params = new URLSearchParams(searchParams);
+    <div className="flex min-w-[200px] max-w-xs flex-col gap-2">
+      <Label>Filter by group leader</Label>
+      <Select
+        onValueChange={handleValueChange}
+        defaultValue={defaultValueSelect}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder="Select a group leader" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="All">All</SelectItem>
 
-        if (groupLeader) params.set("groupLeader", groupLeader);
-        else params.delete("groupLeader");
-        // console.log("params: ", params.toString());
+          <SelectSeparator className="m-[5px] h-px bg-zinc-300" />
 
-        const query = params.size ? "?" + params.toString() : "";
+          <SelectGroup>
+            <SelectLabel>{departmentShortNames[0]}</SelectLabel>
+            {departmentUsersObject[departmentShortNames[0]].map((user) => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.lastName}
+              </SelectItem>
+            ))}
+          </SelectGroup>
 
-        // router.push("/dashboard/grants/list" + query);
-        // Add 'pathname' instead of explicitly adding it so that this comp can be added to all pages;
-        router.push(pathname + query);
-      }}
-      scrollShadowProps={{ isEnabled: false }}
-      showScrollIndicators={true}
-      listboxProps={{
-        className: "max-h-[300px] overflow-y-auto ",
-      }}
-    >
-      <SelectSection title="All" showDivider>
-        <SelectItem key="All" value="All" textValue="All">
-          All
-        </SelectItem>
-      </SelectSection>
+          <SelectSeparator className="m-[5px] h-px bg-zinc-300" />
 
-      <SelectSection title={departmentShortNames[0]} showDivider>
-        {departmentUsersObject[departmentShortNames[0]].map((user) => (
-          <SelectItem
-            key={user.id}
-            value={user.id}
-            textValue={user.lastName ?? ""}
-          >
-            {user.lastName}
-          </SelectItem>
-        ))}
-      </SelectSection>
-      <SelectSection title={departmentShortNames[1]} showDivider>
-        {departmentUsersObject[departmentShortNames[1]].map((user) => (
-          <SelectItem
-            key={user.id}
-            value={user.id}
-            textValue={user.lastName ?? ""}
-          >
-            {user.lastName}
-          </SelectItem>
-        ))}
-      </SelectSection>
-      <SelectSection title={departmentShortNames[2]} showDivider>
-        {departmentUsersObject[departmentShortNames[2]].map((user) => (
-          <SelectItem
-            key={user.id}
-            value={user.id}
-            textValue={user.lastName ?? ""}
-          >
-            {user.lastName}
-          </SelectItem>
-        ))}
-      </SelectSection>
+          <SelectGroup>
+            <SelectLabel>{departmentShortNames[1]}</SelectLabel>
+            {departmentUsersObject[departmentShortNames[1]].map((user) => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.lastName}
+              </SelectItem>
+            ))}
+          </SelectGroup>
 
-      {/* This works fine but gives TS error - instead render each dept's user separately above */}
-      {/* {departmentShortNames.map((department) => (
-        <SelectSection key={department} title={department} showDivider>
-          {departmentUsersObject[department].map((user) => (
-            <SelectItem
-              key={user.id}
-              value={user.id}
-              textValue={user.lastName ?? ""}
-            >
-              {user.lastName}
-            </SelectItem>
-          ))}
-        </SelectSection>
-      ))} */}
+          <SelectSeparator className="m-[5px] h-px bg-zinc-300" />
 
-      {/* <SelectSection title="Individual">
-        {users.map((user) => (
-          <SelectItem
-            key={user.id}
-            value={user.id}
-            textValue={user.lastName ?? ""}
-          >
-            {user.lastName}
-          </SelectItem>
-        ))}
-      </SelectSection> */}
-    </Select>
+          <SelectGroup>
+            <SelectLabel>{departmentShortNames[2]}</SelectLabel>
+            {departmentUsersObject[departmentShortNames[2]].map((user) => (
+              <SelectItem key={user.id} value={user.id}>
+                {user.lastName}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </Select>
+    </div>
   );
+
+  // return (
+  //   <Select
+  //     items={departmentShortNames as Iterable<object>}
+  //     label="Filter by group leader...."
+  //     className="max-w-xs"
+  //     defaultSelectedKeys={[searchParams.get("groupLeader") || "All"]}
+  //     onChange={(event) => {
+  //       const groupLeader = event.target.value;
+  //       // console.log("groupLeader: ", groupLeader);
+  //       const params = new URLSearchParams(searchParams);
+
+  //       if (groupLeader) params.set("groupLeader", groupLeader);
+  //       else params.delete("groupLeader");
+  //       // console.log("params: ", params.toString());
+
+  //       const query = params.size ? "?" + params.toString() : "";
+
+  //       // router.push("/dashboard/grants/list" + query);
+  //       // Add 'pathname' instead of explicitly adding it so that this comp can be added to all pages;
+  //       router.push(pathname + query);
+  //     }}
+  //     scrollShadowProps={{ isEnabled: false }}
+  //     showScrollIndicators={true}
+  //     listboxProps={{
+  //       className: "max-h-[300px] overflow-y-auto ",
+  //     }}
+  //   >
+  //     <SelectSection title="All" showDivider>
+  //       <SelectItem key="All" value="All" textValue="All">
+  //         All
+  //       </SelectItem>
+  //     </SelectSection>
+
+  //     <SelectSection title={departmentShortNames[0]} showDivider>
+  //       {departmentUsersObject[departmentShortNames[0]].map((user) => (
+  //         <SelectItem
+  //           key={user.id}
+  //           value={user.id}
+  //           textValue={user.lastName ?? ""}
+  //         >
+  //           {user.lastName}
+  //         </SelectItem>
+  //       ))}
+  //     </SelectSection>
+  //     <SelectSection title={departmentShortNames[1]} showDivider>
+  //       {departmentUsersObject[departmentShortNames[1]].map((user) => (
+  //         <SelectItem
+  //           key={user.id}
+  //           value={user.id}
+  //           textValue={user.lastName ?? ""}
+  //         >
+  //           {user.lastName}
+  //         </SelectItem>
+  //       ))}
+  //     </SelectSection>
+  //     <SelectSection title={departmentShortNames[2]} showDivider>
+  //       {departmentUsersObject[departmentShortNames[2]].map((user) => (
+  //         <SelectItem
+  //           key={user.id}
+  //           value={user.id}
+  //           textValue={user.lastName ?? ""}
+  //         >
+  //           {user.lastName}
+  //         </SelectItem>
+  //       ))}
+  //     </SelectSection>
+
+  //     {/* This works fine but gives TS error - instead render each dept's user separately above */}
+  //     {/* {departmentShortNames.map((department) => (
+  //       <SelectSection key={department} title={department} showDivider>
+  //         {departmentUsersObject[department].map((user) => (
+  //           <SelectItem
+  //             key={user.id}
+  //             value={user.id}
+  //             textValue={user.lastName ?? ""}
+  //           >
+  //             {user.lastName}
+  //           </SelectItem>
+  //         ))}
+  //       </SelectSection>
+  //     ))} */}
+
+  //     {/* <SelectSection title="Individual">
+  //       {users.map((user) => (
+  //         <SelectItem
+  //           key={user.id}
+  //           value={user.id}
+  //           textValue={user.lastName ?? ""}
+  //         >
+  //           {user.lastName}
+  //         </SelectItem>
+  //       ))}
+  //     </SelectSection> */}
+  //   </Select>
+  // );
 };
 
 const useUsersWithDepartment = () =>
